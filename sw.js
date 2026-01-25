a1333f1fd3aac38a5a3afcf910e051d53ee83ed0
rank-shot <amitkumar15july1995@gmail.com>
2026-01-25 16:07:00 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'dsssb-v1';
+const assets = ['./', './index.html', './style.css', './script.js', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
